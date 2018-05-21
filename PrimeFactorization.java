e7082d1859838414cf52cbce461951a5a10ef015
maxi67 <gckc3130@gmail.com>
2018-05-21 18:33:24 +0800

    Create PrimeFactorization.java

---
+++ PrimeFactorization.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class PrimeFactorization {
+
+	public static void showList(int N) {
+		Queue<Integer> pf = new ArrayDeque<Integer>();
+		double sq = Math.sqrt(N+0.5);
+		for (int i = 2; i < sq; i++) {
+			while (N % i == 0 && N > 1) {
+				pf.add(i);
+				N /= i;
+			}
+		}
+		System.out.println(pf);
+	}
+	
+	public static void showMaxList(int N) {
+		Queue<Integer> pfm = new ArrayDeque<Integer>();
+		double sq = Math.sqrt(N+0.5);
+		for (int i = 2; i < sq; i++) {
+			if (N % i == 0) {
+				int factor = 1;
+				while (N % i == 0 && N > 1) {
+					factor *= i;
+					N /= i;
+				}
+				pfm.add(factor);
+			}
+		}
+		System.out.println(pfm);
+	}
+	
+	public static void main(String args[]) {
+		int N = 72;
+		showList(N);
+		showMaxList(N);
+	}
+}
